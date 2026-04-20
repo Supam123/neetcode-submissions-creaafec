5e74b3de937b248b5d25161acbf2e044278c642e
Supam123 <44885608+Supam123@users.noreply.github.com>
2026-04-20 13:53:58 -0400

    Add: subsets - submission-3

---
+++ Data Structures & Algorithms/subsets/submission-3.py
@@ -0,0 +1,33 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        # here order does not matter
+        # [1,2] is same as [2,1] order does not matter 
+        # pick and skip
+        
+        '''
+        my subset will keep on appendin g elements and eventually i will hit base case so we
+        append it to res and trhen now subest is already 1,2,3 so we need to calculate other
+        paths with other decision trees. we say hmm actually let me clear it out subset
+        '''
+        
+        res = []
+        subset = []
+        def dfs(i):
+            if i == len(nums): 
+                return res.append(subset.copy())
+            
+            subset.append(nums[i]) #we take nums[i]
+            dfs(i+1)
+
+            subset.pop() # we can pop nums[i]
+            dfs(i+1)
+        dfs(0)
+        return res
+        '''
+        so we go we add 1 to sub set 2 to subset 3 to subset and then we reach base case
+        we append it to res then we after 3 returns cuz  3+1 is 4 we reutn we pop 3 from subset and 
+        call dfs(4) again this time we append subset was [1,2] so we append that ti res
+        then dfs
+        '''
+
+        

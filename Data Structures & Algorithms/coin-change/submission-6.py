53f2fd93246dff06efa15e027a186d62d7c25f8b
Supam123 <44885608+Supam123@users.noreply.github.com>
2026-05-19 16:00:07 -0400

    Add: coin-change - submission-6

---
+++ Data Structures & Algorithms/coin-change/submission-6.py
@@ -0,0 +1,45 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        memo = {}
+        def dfs(amt):
+            if amt == 0:
+                return  0
+            if amt < 0 :
+                return float('inf')
+            if amt in memo:
+                return memo[amt]
+            res = float('inf')
+            for coin in coins:
+                res =  min(res,1+dfs(amt-coin))
+            
+            memo[amt] = res
+            return memo[amt]
+        minCoins = dfs(amount)
+        return -1 if minCoins >= 1e9 else minCoins
+
+
+        
+
+
+
+
+        # memo = {}
+        # def dfs(i,amt):
+        #     if  amt == 0:
+        #         return 0
+        #     if amt < 0:
+        #         return float('inf') # invalid path return impossible
+        #     if i == len(coins):
+        #         return float('inf')
+        #     if (i,amt) in memo:
+        #         return memo[(i,amt)]
+            
+        #     skip_res = dfs(i+1,amt)
+        #     take_res = 1 + dfs(i, amt-coins[i])
+        #     res = min(skip_res,take_res)
+        #     memo[(i,amt)] = res
+        #     return res
+        # return -1 if dfs(0,amount) == float('inf')   else  dfs(0,amount)
+
+        
+        

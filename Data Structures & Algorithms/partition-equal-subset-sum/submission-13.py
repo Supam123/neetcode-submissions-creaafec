d990b2c7b2975fff57c87b83b65cfa6d6df68c8a
Supam123 <44885608+Supam123@users.noreply.github.com>
2026-06-10 09:53:03 -0400

    Add: partition-equal-subset-sum - submission-13

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-13.py
@@ -0,0 +1,22 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        target = sum(nums) / 2
+        if sum(nums) %2 != 0:
+            return False
+        memo = {}
+        def dp(i,capacity):
+            if capacity == 0:
+                return True
+            if i == len(nums) and capacity != 0:
+                return False
+            if (i,capacity) in memo:
+                return memo[(i,capacity)]
+            
+            take = dp(i+1,capacity-nums[i])
+            skip = dp(i+1,capacity)
+            memo[(i,capacity)] = take or skip
+            return memo[(i,capacity)]
+        return dp(0,target)
+
+  
+    
